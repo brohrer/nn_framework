6cf6c386b0acb1f1433674ee45269906e6def98b
brohrer <brohrer@gmail.com>
2019-08-07 11:39:26 -0400

    normalize() removed

--- nn_framework/framework.py
+++ nn_framework/framework.py
@@ -1,36 +1,20 @@
-import os
 import numpy as np
-import matplotlib.pyplot as plt
-plt.switch_backend("agg")
 
 
 class ANN(object):
     def __init__(
         self,
         model=None,
-        expected_range=(-1, 1),
     ):
         self.layers = model
         self.n_iter_train = int(1e8)
         self.n_iter_evaluate = int(1e6)
-        self.expected_range = expected_range
 
     def train(self, training_set):
         for i_iter in range(self.n_iter_train):
-            x = self.normalize(next(training_set()).ravel())
+            x = next(training_set()).ravel()
             print(x)
 
     def evaluate(self, evaluation_set):
         for i_iter in range(self.n_iter_evaluate):
-            x = self.normalize(next(evaluation_set()).ravel())
-
-    def normalize(self, values):
-        """
-        Transform the input/output values so that they tend to
-        fall between -.5 and .5
-        """
-        min_val = self.expected_range[0]
-        max_val = self.expected_range[1]
-        scale_factor = max_val - min_val
-        offset_factor = min_val
-        return (values - offset_factor) / scale_factor - .5
+            x = next(evaluation_set()).ravel()
--- run_framework.py
+++ run_framework.py
@@ -4,14 +4,9 @@ import nn_framework.framework as framework
 training_set, evaluation_set = dat.get_data_sets()
 
 sample = next(training_set())
-input_value_range = (0, 1)
 n_pixels = sample.shape[0] * sample.shape[1]
-
 n_nodes = [n_pixels, n_pixels]
 
-autoencoder = framework.ANN(
-    model=None,
-    expected_range=input_value_range,
-)
+autoencoder = framework.ANN(model=None)
 autoencoder.train(training_set)
 autoencoder.evaluate(evaluation_set)
