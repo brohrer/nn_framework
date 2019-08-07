c898d3270e0879394dec29784aafa66b1c5d164f
brohrer <brohrer@gmail.com>
2019-08-07 11:48:29 -0400

    runner removed

--- run_framework.py
+++
@@ -1,7 +0,0 @@
-import data_loader_two_by_two as dat
-
-training_set, evaluation_set = dat.get_data_sets()
-
-sample = next(training_set())
-n_pixels = sample.shape[0] * sample.shape[1]
-n_nodes = [n_pixels, n_pixels]
