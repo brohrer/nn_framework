6aa216e31ee2a985ea954154f8df19e3dbdb1e7a
brohrer <brohrer@gmail.com>
2019-08-07 11:50:37 -0400

    data generators removed

--- data_loader_two_by_two.py
+++ data_loader_two_by_two.py
@@ -76,15 +76,3 @@ def get_data_sets():
             [1, 0]
         ]),
     ]
-
-    def training_set():
-        while True:
-            index = np.random.choice(len(examples))
-            yield examples[index]
-
-    def evaluation_set():
-        while True:
-            index = np.random.choice(len(examples))
-            yield examples[index]
-
-    return training_set, evaluation_set
