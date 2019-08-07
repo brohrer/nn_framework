c16027c08fed591bcc45c12d09d9d6902f939aad
brohrer <brohrer@gmail.com>
2019-08-07 11:42:03 -0400

    training and evaluation reduced to wireframe

--- nn_framework/framework.py
+++ nn_framework/framework.py
@@ -7,14 +7,9 @@ class ANN(object):
         model=None,
     ):
         self.layers = model
-        self.n_iter_train = int(1e8)
-        self.n_iter_evaluate = int(1e6)
 
     def train(self, training_set):
-        for i_iter in range(self.n_iter_train):
-            x = next(training_set()).ravel()
-            print(x)
+        pass
 
     def evaluate(self, evaluation_set):
-        for i_iter in range(self.n_iter_evaluate):
-            x = next(evaluation_set()).ravel()
+        pass
